936d830b29a392724c2813fedf91333f7fc5b700
Chitravansh <mchitravansh@gmail.com>
2025-10-20 10:57:07 +0530

    first commit

---
+++ feed.py
@@ -0,0 +1,41 @@
+from fastmcp import FastMCP
+import feedparser
+
+mcp = FastMCP(name = "FreeCodeCamp Feed Searcher ")
+
+@mcp.tool()
+def fcc_news_search(query: str, max_results: int = 3):
+    """
+    Search FreeCodeCamp news feed via RSS by title description.
+
+    """
+
+    feed = feedparser.parse("https://www.freecodecamp.org/news/rss")
+    results = []
+    query_lower = query.lower()
+    for entry in feed.entries:
+        title = entry.get("title","")
+        description =  entry.get("description","")
+        if query_lower in title.lower() or query_lower in description.lower():
+            results.append({"title": title, "url": entry.get("link","")})
+        if len(results) >= max_results:
+            break 
+            
+    return results or [{"message": "No results"}]
+
+@mcp.tool()
+def fcc_youtube_search(query: str, max_results: int = 3):
+    """Search freecodcamp Youtube channel via RSS by title/description"""
+    feed = feedparser.parse("https://www.youtube.com/feeds/videos.xml?channel_id=UCbTLwN10NoCU4WDzLf1JMOA")
+    results = []
+    query_lower = query.lower()
+    for entry in feed.entries:
+        title = entry.get("title","")
+        if query_lower in title.lower():
+            results.append({"title":title, "url":entry.get("link","")})
+        if len(results) >= max_results:
+            break 
+    return results or [{"message":"No videos found"}]
+
+if __name__ == "__main__":
+    mcp.run(transport="http")
